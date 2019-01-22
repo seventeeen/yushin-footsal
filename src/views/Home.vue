b751fb2d8fa2f57fcd2f03afc88130f591eb0d34
seventeen <powdragon1@gmail.com>
2019-01-22 17:07:19 +0900

    일단
    Signed-off-by: seventeen <powdragon1@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
+    <div>여기가 홈입니다</div>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="유신 어학반 풋살 포럼 ^^"/>
+    <HelloWorld msg="여기는 다른 페이지입니다"/>
   </div>
 </template>
 
